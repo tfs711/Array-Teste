f6e1f5114ebb33c7fa9e2723f87f15f3e63bec7c
Tiago <https://github.com/tfs711/Array-Teste.git>
2021-03-09 21:51:28 -0300

    teste Lista

--- src/main/kotlin/one/digitalinovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinovation.collections
 
 fun main(){
-    val Joao = Funcionario("João", 1500.0)
-    val Maria = Funcionario("Maria", 1000.0)
-    val Jose = Funcionario("José", 2000.0)
+    val Joao = Funcionario("João", 1500.0, "CLT")
+    val Maria = Funcionario("Maria", 1000.0, "CLT")
+    val Jose = Funcionario("José", 2000.0, "PJ")
 
     val funcionarios = listOf(Joao, Maria, Jose)
 
@@ -14,11 +14,16 @@ fun main(){
 
     println("----------------------")
     funcionarios.sortedBy { it.salario }.forEach { println(it) } //buscando somente o salario
+
+    println("----------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) } //agrupar elementos
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
