749b0ad747cca4733d789bd3e659fee7096aeffe
Tiago <https://github.com/tfs711/Array-Teste.git>
2021-03-09 21:14:48 -0300

    teste Operaçoes

--- src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
@@ -16,4 +16,16 @@ fun main(){
 
     val salariosMaiorque2500 = salarios.filter { it > 2500.0 }
     salariosMaiorque2500.forEach{println(it)}
+
+    println("--------------------------------------")
+
+    println(salarios.count { it in 1000.0..2500.0})  //count - contou qts expressões estão entre eles - 1000 e 2500
+
+    println("--------------------------------------")
+
+    println(salarios.find { it == 3000.0})  // ele procura o valor pedido, se for um elemento inexistente, retornaria null
+
+    println("--------------------------------------")
+
+    println(salarios.any { it == 1000.0})  // ele procura qualquer elemento válido, retornando true, e senão, false
 }
